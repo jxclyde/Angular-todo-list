0a37b5243ca09fc4a8ae5424e5fd68da1b6728c4
Roman Onyshkiv <roman.onushkiv@gmail.com>
2023-06-03 01:15:15 +0300

    Adding and Removing specific tasks using binding.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,31 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+  
 export class AppComponent {
   title = 'todo-list';
+  tasks = [
+    "Visit Ann",
+    "Call dad",
+    "Go to the gym",
+    "Wash the car",
+    "Shop for the party"
+  ]
+
+  add(newTask: string)
+  {
+    this.tasks.push(newTask);
+  }
+
+  remove(existingTask: string)
+  {
+  var userConfirmed = confirm(`Are you sure you want to delete this task? \n "${existingTask}"`); 
+
+    if (userConfirmed)
+    { 
+    this.tasks = this.tasks.filter(task => task !== existingTask);
+    }
+  }
+
+
 }
